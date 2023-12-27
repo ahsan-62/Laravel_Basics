ddfa6102e3caf98336decf76323f276c4b7a009f
ahsan-62 <ahsan.cse.101792@gmail.com>
2023-12-28 00:38:10 +0600

    Required Route and Optional Route parameter

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
 
 Route::get('/about',function(){
 
     return view('about');
-});
+})->name('about');
 
 Route::get('/contact',function(){
 
 return view('contact');
 
-});
+})->name('contact');
 
-Route::get('/service',function(){
+Route::get('/service/{service_id}',function($service_id){
 
 return view('service');
 
-});
+})->name('service');
