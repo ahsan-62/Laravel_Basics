75d9b03ce6cd3f561e2f9bd334cfc2a59cea674f
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-01-02 13:02:44 +0600

    category Error show ,validation,model,request

--- resources/views/Category/create.blade.php
+++ resources/views/Category/create.blade.php
@@ -9,12 +9,34 @@ Create Form-Category
         <form action="{{ route('category.store') }}" method="POST">
             @csrf
             <div class="form-group mb-3">
-                <label class="mb-3" for="name">Category Name</label>
-                <input type="text" class="form-control" name="category_name" id="name" aria-describedby="emailHelp" placeholder="Enter Category Name">
+                <label class="mb-3" for="category_name">Category Name</label>
+                <input type="text" class="form-control @error('category_name')
+                is-invalid
+                @enderror" name="category_name" id="category_name" placeholder="Enter category Name">
+
+                @error('category_name')
+
+                <span class="invalid-feedback" role="alert">
+
+                    <strong>{{ $message }}</strong>
+
+                </span>
+
+                @enderror
+
             </div>
             <div class="form-group mb-3">
-                <label class="mb-3" for="slug">Category Slug</label>
-                <input type="text" class="form-control" id="slug" name="category_slug" aria-describedby="emailHelp" placeholder="Enter Category Slug">
+                <label class="mb-3" for="category_slug">Category Slug</label>
+                <input type="text" class="form-control @error('category_slug')
+                is-invalid
+                @enderror" id="category_slug" name="category_slug" aria-describedby="emailHelp" placeholder="Enter Category Slug">
+                @error('category_slug')
+
+                <span class="invalid-feedback " role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+
+                @enderror
             </div>
 
               <div class="form-check">
